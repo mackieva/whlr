53f56b746c9939d679a127ea4b4110a1f65e77d2
Ryan McGuire <ryan.mcguire.va@gmail.com>
2022-07-10 15:24:41 -0400

    getting there
    Signed-off-by: Ryan McGuire <ryan.mcguire.va@gmail.com>

--- front-page.php
+++ front-page.php
@@ -76,7 +76,7 @@ function kreativ_front_page_widgets() {
 						<li class="splide__slide">
 								<div class="row">
 									<div class="col-md-6 splide__slide--imgContainer">
-										<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/themes/genesis-sample/images/img-demo.jpg" alt="Eplore Properties" class="laxx">
+										<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/themes/genesis-sample/images/img-demo.jpg" alt="Explore Properties - Village of Martinsville" class="laxx">
 										<div class="imgPlaceholder"></div>
 									</div>
 									<div class="col-md-6 d-flex justify-content-center align-items-center">
@@ -94,7 +94,7 @@ function kreativ_front_page_widgets() {
 						<li class="splide__slide">
 								<div class="row">
 									<div class="col-md-6 splide__slide--imgContainer"">
-										<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/themes/genesis-sample/images/img-demo.jpg" alt="Eplore Properties" class="laxx">
+										<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/uploads/ft-howard.jpg" alt="Explore Properties - t. Howard Square" class="laxx">
 										<div class="imgPlaceholder"></div>
 									</div>
 									<div class="col-md-6 d-flex justify-content-center align-items-center">
@@ -112,7 +112,7 @@ function kreativ_front_page_widgets() {
 						<li class="splide__slide">
 								<div class="row">
 									<div class="col-md-6 splide__slide--imgContainer"">
-										<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/themes/genesis-sample/images/img-demo.jpg" alt="Eplore Properties" class="laxx">
+										<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/uploads/franklin-fillage.jpg" alt="Explore Properties - Franklin Village Shopping Center" class="laxx">
 										<div class="imgPlaceholder"></div>
 									</div>
 									<div class="col-md-6 d-flex justify-content-center align-items-center">
@@ -210,7 +210,7 @@ function kreativ_front_page_widgets() {
 					</div>
 				</div>
 				<div class="col-md-6 homeSection__imgWrapper" data-scroll>
-					<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/themes/genesis-sample/images/img-demo.jpg" alt="Eplore Properties" class="laxx homeSection__img">
+					<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/themes/genesis-sample/images/img-demo-2.jpg" alt="Eplore Properties" class="laxx homeSection__img">
 					<div class="imgPlaceholder"></div>
 				</div>
 			</div>
@@ -241,7 +241,7 @@ function kreativ_front_page_widgets() {
 		<section class="homeSection" data-scroll-section>
 			<div class="row">
 				<div class="col-md-6 homeSection__imgWrapper" data-scroll>
-					<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/themes/genesis-sample/images/img-demo.jpg" alt="Eplore Properties" class="laxx homeSection__img">
+					<img src="<?php echo get_bloginfo('wpurl') ?>/wp-content/themes/genesis-sample/images/img-demo-3.jpg" alt="Eplore Properties" class="laxx homeSection__img">
 					<div class="imgPlaceholder"></div>
 				</div>
 				<div class="col-md-6 d-flex justify-content-center align-items-center">
